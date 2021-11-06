77b0d2b6fe25489140cacdcd08ab67122ef8243f
Rafał Mikołajun <root@rmweb.pl>
2021-11-06 17:21:19 +0100

    add pagination to products

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -5,22 +5,36 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductsController extends AbstractFOSRestController
 {
     private ProductRepository $productRepository;
+    private PaginatorInterface $paginator;
 
-    public function __construct(ProductRepository $productRepository)
+    /**
+     * @param ProductRepository $productRepository
+     * @param PaginatorInterface $paginator
+     */
+    public function __construct(ProductRepository $productRepository, PaginatorInterface $paginator)
     {
         $this->productRepository = $productRepository;
+        $this->paginator = $paginator;
     }
 
     #[IsGranted('ROLE_PRODUCTS_CATALOG_ACCESS')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->handleView($this->view($this->productRepository->findAll()));
+        $pagination = $this->paginator->paginate(
+            $this->productRepository->createQueryBuilder('p')->orderBy('p.name', 'ASC'),
+            $request->query->getInt('page', 1),
+            $this->getParameter('api_items_per_page')
+        );
+
+        return $this->handleView($this->view($pagination->getItems()));
     }
 
     #[IsGranted('ROLE_PRODUCTS_CATALOG_ACCESS')]
